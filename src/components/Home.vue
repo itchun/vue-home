1dd5c8225ebce055dfe274a8b163b3931d1ac4d9
itchun <itchun@163.com>
2020-12-14 11:38:12 +0800

    frist commit

---
+++ src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-navigation></v-navigation>
+    <div id="main_home">
+      <div id="carousel">
+        <slider :pages="pages" :sliderinit="sliderinit"></slider>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/navigation/Navigation.vue'
+import slider from 'vue-concise-slider'// 引入slider组件
+
+export default {
+  name: 'home',
+  el: '#app',
+  data () {
+    return {
+      //图片列表[arr]
+      pages: [
+        {
+          title: '',
+          style: {
+            background: 'url(src/assets/carousel/1.jpg)'
+          }
+        },
+        {
+          title: '',
+          style: {
+            background: 'url(src/assets/carousel/2.jpg)'
+          }
+        },
+        {
+          title: '',
+          style: {
+            background: 'url(src/assets/carousel/3.jpg)'
+          }
+        },
+        {
+          title: 'slide3',
+          style: {
+            background: '#4bbfc3',
+          },
+        }
+      ],
+      //滑动配置[obj]
+      sliderinit: {
+        currentPage: 0,//当前页码
+        thresholdDistance: 500,//滑动判定距离
+        thresholdTime: 100,//滑动判定时间
+        autoplay: 1000,//自动滚动[ms]
+        loop: true,//循环滚动
+        direction: 'vertical',//方向设置，垂直滚动
+        infinite: 1,//无限滚动前后遍历数
+        slidesToScroll: 1,//每次滑动项数
+      }
+    }
+  },
+  components: {
+    'v-navigation': Navigation,
+    'slider': slider
+  }
+}
+</script>
+
+<style>
+
+#main_home {
+ /* left: 25%;
+  right: 20%;*/
+}
+
+</style>
---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="navigation">
+    <div class="navigation_inner">
+      <a class="logo_info" href="">{{ logo_info }}</a>
+      <a class="home 1" href="">{{ home_1 }}</a>
+      <a class="home 2" href="">{{ home_2 }}</a>
+      <a class="home 3" href="">{{ home_3 }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      logo_info: '王春的个人网站',
+      home_1: '主页',
+      home_2: '博客',
+      home_3: '留言板'
+    }
+  }
+}
+
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#navigation {
+  background-image: url("../../assets/navigation/backImage.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100px;
+}
+
+.navigation_inner {
+  position: absolute;
+  top: 20px;
+  left: 25%;
+  right: 20%;
+  font-family: 'Microsoft Himalaya';
+}
+
+.logo_info{
+  font-size: 40px;
+}
+
+.home{
+  font-size: 30px;
+  margin: 55px;
+}
+
+a{
+  text-decoration: none;
+  color: #000000;
+}
+</style>
